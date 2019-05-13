716ac28ac4ae1ef2c70c62eca4cbb99d0844e409
unknown <9556354+miashore@users.noreply.github.com>
2019-05-13 10:45:34 -0700

    adjusted positioning and sizing of high score

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -11,7 +11,6 @@ public class ScoreManager : MonoBehaviour {
     public Text highScoreValue;
 
     // RectTransform to change Text position
-    public RectTransform scoreTransform;
     public RectTransform highScoreTransform;
 
     void Awake () {
@@ -24,11 +23,6 @@ public class ScoreManager : MonoBehaviour {
     void Update () {
         // Displays the score value in the Text component
         scoreValue.text = score.ToString ();
-        if (scoreValue.text.Length > 10) {
-            // Shrinks the font and moves the display down as the number gets larger
-            scoreValue.fontSize = 30;
-            scoreTransform.anchoredPosition = new Vector3 (137.2f, -75f, 0f);
-        }
 
         // Sets the high score
         if (score > highScore) {
@@ -38,10 +32,10 @@ public class ScoreManager : MonoBehaviour {
             if (highScoreValue.text.Length > 4) {
                 // Shrinks the text and moves it down to fit in the space for larger scores
                 highScoreValue.fontSize = 30;
-                highScoreTransform.anchoredPosition = new Vector3 (115.6f, -31f, 0);
-            } else if (highScoreValue.text.Length > 2) {
+                highScoreTransform.anchoredPosition = new Vector3 (6.104f, -31f, 0);
+            } else if (highScoreValue.text.Length > 6) {
                 highScoreValue.fontSize = 20;
-                highScoreTransform.anchoredPosition = new Vector3 (115.6f, -39f, 0);
+                highScoreTransform.anchoredPosition = new Vector3 (6.104f, -39f, 0);
             }
 
             // Stores the players's high score 
